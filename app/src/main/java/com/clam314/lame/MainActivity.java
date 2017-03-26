27737ef7d640599622dd0c9192eb14b88906f8d2
clam314 <clam314@163.com>
2017-03-27 00:01:56 +0800

    更改样式

--- app/src/main/java/com/clam314/lame/MainActivity.java
+++ app/src/main/java/com/clam314/lame/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.Toast;
 
